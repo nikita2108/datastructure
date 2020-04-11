f7ed273eb7017739dad03d60ce2188d6049ebfbf
nikita2108 <62983464+nikita2108@users.noreply.github.com>
2020-04-11 13:50:43 +0530

    created datastructure

---
+++ datastructure.py
@@ -0,0 +1,22 @@
+my_list=[3,8,1,6,0,8,4]
+print(my_list.index(8))
+print(my_list.count(8))
+my_list.sort()
+print(my_list)
+my_list.reverse()
+print(my_list)
+
+
+
+my_data=("hi","hello","bye")
+print(my_data)
+my_data2=(1,2.8,"hello world")
+print(my_data2)
+my_data3=("book",[1,2,3])
+print(my_data3)
+my_data4=((2,3,4),(1,2,"hi"))
+print(my_data4)
+
+my_Dict={'A':'Application','C':'cat','B':'Boy'}
+del my_Dict["A"]
+print(my_Dict)
